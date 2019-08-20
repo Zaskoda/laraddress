6f5e3ac11bc0a17f963f7d406f74fac2c5611337
zasKoda <zaskoda@gmail.com>
2019-08-19 23:58:31 -0700

    more fixes

--- app/Http/Requests/PhoneNumberRequest.php
+++ app/Http/Requests/PhoneNumberRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class IdentityRequest extends FormRequest
+class PhoneNumberRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -24,7 +24,7 @@ class IdentityRequest extends FormRequest
     public function rules()
     {
         return [
-            'email_address' => 'required'
+            'number' => 'required'
         ];
     }
 }
