c9fc9a90779bb037da19225f435c18faaced9a87
zasKoda <zaskoda@gmail.com>
2019-08-21 18:43:23 -0700

    better address book

--- resources/views/partials/contact_row.blade.php
+++ resources/views/partials/contact_row.blade.php
@@ -1,19 +1,23 @@
-<div class="row">
-    <div class="col-sm">
-        <div>@include('partials.row_formal_name')</div>
-        <div>@include('partials.row_nickname')</div>
-        <div>@include('partials.row_birthday')</div>
-    </div>
-    <div class="col-sm">
-        @include('partials.row_postal_address')
-    </div>
-    <div class="col-sm">
-        @include('partials.row_phone_number')
-    </div>
-    <div class="col-sm">
-        @include('partials.row_email_account')
-    </div>
-    <div class="col-sm">
-        @include('partials.row_siren_platform')
+<div class="card">
+    <div class="mt-1 mb-1">
+        <div class="row">
+            <div class="col-sm">
+                <div>@include('partials.row_formal_name')</div>
+                <div>@include('partials.row_nickname')</div>
+                <div>@include('partials.row_birthday')</div>
+            </div>
+            <div class="col-sm">
+                @include('partials.row_postal_address')
+            </div>
+            <div class="col-sm">
+                @include('partials.row_phone_number')
+            </div>
+            <div class="col-sm">
+                @include('partials.row_email_account')
+            </div>
+            <div class="col-sm">
+                @include('partials.row_siren_platform')
+            </div>
+        </div>
     </div>
 </div>
