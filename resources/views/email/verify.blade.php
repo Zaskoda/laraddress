6790ab2e15e5733b157579857b0df1fec61fd2bd
zasKoda <zaskoda@gmail.com>
2019-09-08 22:23:54 -0700

    Better grammar

--- resources/views/email/verify.blade.php
+++ resources/views/email/verify.blade.php
@@ -1,18 +1,16 @@
 <p>Hello,</p>
 
-<p>You or someone using your email address attempted to access address book!</p>
+<p>You or someone using your email address attempted to access {{ $adminName }}'s address book!</p>
 
-<p>To finish you login process, click this <a href="{{ $tokenlink }}">Verification Link</a></p>
+<p>To finish your login process, click this <a href="{{ $tokenlink }}">Verification Link</a></p>
 
 <p>In case that link did not display correctly in your email client, you can copy and paste the following link:<p>
 
 <p>{{ $tokenlink }}</p>
 
-<p>Once you log in (or if you request another verification link) the link in this email will no longer be valid.</p>
-
 <p>Thank you for keeping me up to date!</p>
 
-
+- {{ $adminName }}
 
 
 
